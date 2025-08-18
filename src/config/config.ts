ecc0e1e9227bb573cf02bf790658f1b1e5857d41
LuckedCoronet45 <lc45@lc-studios.net>
2025-08-18 19:35:38 +0900

    Update pack config type

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,21 +1,37 @@
 import type { CliArgs } from "@/types";
+import type * as esbuild from "esbuild";
+
+export type BehaviorPackScriptsConfig = {
+	entry: string;
+	bundle?: boolean;
+	minify?: boolean;
+	sourceMap?: boolean;
+	tsconfig?: string;
+	esbuildOptionOverrides?: esbuild.CommonOptions;
+};
 
 export type BehaviorPackConfig = {
 	type: "behavior";
 	srcDir: string;
 	outDir: string;
-	manifest: string;
+	manifest: Record<string, unknown>;
 	name?: string;
 	clean?: boolean;
+	scripts?: BehaviorPackScriptsConfig;
+	include?: string[];
+	exclude?: string[];
 };
 
 export type ResourcePackConfig = {
 	type: "resource";
 	srcDir: string;
 	outDir: string;
-	manifest: string;
+	manifest: Record<string, unknown>;
 	name?: string;
 	clean?: boolean;
+	generateTextureList?: boolean;
+	include?: string[];
+	exclude?: string[];
 };
 
 export type PackConfig = BehaviorPackConfig | ResourcePackConfig;
